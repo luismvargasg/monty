700b4ae22b2fc3896d1615170a95b00706b505e2
luismvargasg <1610@holbertonschool.com>
2020-05-13 05:53:51 +0000

    deleting unused functions

--- monty_exec.c
+++ monty_exec.c
@@ -21,22 +21,6 @@ void execute(char **token, stack_t *stack, unsigned int line_num)
 	unsigned int i = 0;
 
 	instruction_t op[] = {
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{"sub", sub},
-		{"div", div},
-		{"mul", mul},
-		{"mod", mod},
-		{"pchar", pchar},
-		{"pstr", pstr},
-		{"rotl", rotl},
-		{"rotr", rotr},
-		{"stack", stack},
-		{"queue", queue},
 		{"null", NULL},
 	};
 	while (op[i].opcode != NULL)
